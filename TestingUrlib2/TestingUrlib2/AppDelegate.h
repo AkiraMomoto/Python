e682f81772dd274faad28e34c9e6cf21f53cd69c
AkiraMomoto <lm2817@nyu.edu>
2014-07-08 11:25:24 -0400

    Initial Commit

---
+++ TestingUrlib2/TestingUrlib2/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TestingUrlib2
+//
+//  Created by Levi Mollison on 7/8/14.
+//  Copyright (c) 2014 AkiraMomoto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TestingUrlib2/TestingUrlib2/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TestingUrlib2
+//
+//  Created by Levi Mollison on 7/8/14.
+//  Copyright (c) 2014 AkiraMomoto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
